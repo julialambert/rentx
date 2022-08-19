3eb07e51a2d4a8f1b94e67d56f86685102c7e763
Júlia Lambert <julialambert@MacBook-Pro-de-Julia.local>
2022-08-18 21:21:04 -0300

    feat: adding profile screen and watermelonDB

---
+++ ios/rentx-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  rentx-Bridging-Header.h
+//  rentx
+//
+//  Created by Júlia Lambert on 18/08/22.
+//
+
+#ifndef rentx_Bridging_Header_h
+#define rentx_Bridging_Header_h
+
+
+#endif /* rentx_Bridging_Header_h */
---
+++ ios/wmelon.swift
@@ -0,0 +1,8 @@
+//
+//  wmelon.swift
+//  rentx
+//
+//  Created by Júlia Lambert on 18/08/22.
+//
+
+import Foundation
